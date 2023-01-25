9b0216fff34c94a28f1f25cd36fae5e6d4b0bfdc
qxxomer <121766059+qxxomer@users.noreply.github.com>
2023-01-25 21:16:18 +0300

    Update reklam.js

--- reklam.js
+++ reklam.js
@@ -54,3 +54,19 @@ window.document.getElementById("n59f").parentNode.removeChild(window.document.ge
                                 ''
                                 ':'
                                 757 b3fc '},b=this,e=b.getElementBy(0,"script"),d=e.length-1,c,g,f,k;h.write=null;for(h.writeln=null;0<=d;--d)if(c=e[d].src.substr(7,20),a[c]!==m){f=h.createElement("script");f.type="text/javascript";f.src=e[d].src;g=a[c];l[g]=m;f.onload=f.onreadystatechange=function(){k=this;l[g]!==m||k.readyState&&"loaded"!==k.readyState&&"complete"!==k.readyState||(l[g]=f.onload=f.onreadystatechange=null,e[0].parentNode.removeChild(f))};e[0].parentNode.insertBefore(f,e[0]);b.deferExecution(function(){if(l[g]===m)return b.displayMessage(f.src);b.nextFunction()});return}b.nextFunction()},u:function(){var a=".cz/bannery/,.gg/ads/,/ad-units.,/ads/a.,/adver_hor.,/aff/banners/ad,/generateAds.,/new_ads/ad,/newaff/float,_adhesion.".split(","),b=this,e=b.getElementBy(0,"img"),d,c;e[0]!==m&&e[0].src!==m&&(d=new Image,d.onload=function(){c=this;c.onload=null;c.onerror=function(){p=null;b.displayMessage(c.src)};c.src=e[0].src+"#"+a.join("")},d.src=e[0].src);b.deferExecution(function(){b.nextFunction()})},nextFunction:function(){var a=p[0];a!==m&&(p.shift(),this[a]())}};l.n59f=n59f=new n;h.addEventListener?l.addEventListener("load",n,!1):l.attachEvent("onload",n)})(window);
+
+
+
+// Reklam Engellemeyi Engelle
+! function() {
+    var b = document.createElement("script");
+    b.type = "text/javascript";
+    b.async = !0;
+    b.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7758652662183132";
+    b.onerror = function() {
+        f();
+        window.adblock = !0
+    };
+    var e = document.getElementsByTagName("script")[0];
+    e.parentNode.insertBefore(b, e)
+}();
